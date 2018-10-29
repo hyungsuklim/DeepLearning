3b285c8ab14bff9997adc4db978b40de2b553d4c
HyungSukLim <gghknb@naver.com>
2018-10-29 11:23:21 +0900

    completed Assignment3

--- Assignment3/captioning.py
+++ Assignment3/captioning.py
@@ -7,18 +7,198 @@
 import tensorflow as tf
 import numpy as np
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 
 class Captioning():
-	
-	def __init__(self):
-		self._start = 1
 
+    def __init__(self, batch_size,is_training):
+        self._start = 1
+        
+        self.is_training = is_training
+        
+        # for TRAINING
+        self.epoch = 150
+        self.batch_size = batch_size
+        
+        self.n_hidden = 512
+        
+        self.n_words = 1004
+        self.maxlen = 17
+        self.dim_features = 512
+        self.embedding_size = 512
+        
+        self.initializer = tf.random_uniform_initializer(minval= -0.08, maxval=  0.08)
+    
+    def build_model(self):
+         with tf.Graph().as_default() as self.graph:
+            #loss = 0
+            self.global_step_tensor = tf.contrib.framework.get_or_create_global_step()
+ 
+            if self.is_training:
+                self.img_features = tf.placeholder(tf.float32, shape=[self.batch_size, self.dim_features]) # img_features
+                self.input_seqs = tf.placeholder(tf.int32, shape=[self.batch_size, self.maxlen - 1]) # captions[:, :16]
+                self.target_seqs = tf.placeholder(tf.int32, shape=[self.batch_size, self.maxlen - 1]) # captions[:, 1:]
+                self.input_mask = tf.placeholder(tf.int32, shape=[self.batch_size, self.maxlen - 1]) # mask = 1*(captions[:, 16]>0)
+            else:
+                self.img_features = tf.placeholder(tf.float32, shape=[1, self.dim_features])
+                self.input_seqs = tf.placeholder(tf.int32, shape=[1, 1])
+                self.batch_size = 1
+            
+            
+            # IMAGE EMBEDDING
+            '''
+            with tf.variable_scope("image_embedding") as scope:
+                self.image_embedding = tf.contrib.layers.fully_connected(
+                                            inputs=self.img_features,
+                                            num_outputs = self.embedding_size,
+                                            activation_fn = None,
+                                            weights_initializer = self.initializer,
+                                            biases_initializer = None,
+                                            scope = scope)
+            '''                              
+            self.image_embeddings = self.img_features #self.image_embedding # NEW IMAGE EMBEDDINGS
+            # (500, 512)
+            
+            # SEQ EMBEDDING
+            with tf.variable_scope('seq_embedding'):#, tf.device('/cpu:0'):
+                # 'get_variable' can get existing values.
+                self.embedding_map = tf.get_variable(name = 'map',
+                                                shape = [self.n_words, self.embedding_size],
+                                                initializer = self.initializer)
+            self.seq_embeddings = tf.nn.embedding_lookup(self.embedding_map, self.input_seqs) # NEW SEQ EMBEDDINGS
+            # (500, 16, 512)
+            
+            
+            # LSTM MODEL
+            self.lstm_cell = tf.contrib.rnn.BasicLSTMCell(num_units = self.n_hidden, state_is_tuple = True);
+            
+            if self.is_training is True:
+                self.lstm_cell = tf.contrib.rnn.DropoutWrapper(self.lstm_cell, input_keep_prob=0.7, output_keep_prob=0.7)
+                
+            with tf.variable_scope('lstm', initializer=self.initializer) as lstm_scope:
+                self.zero_state = self.lstm_cell.zero_state(batch_size = self.batch_size, dtype = tf.float32)
+                # PREDICT PHASE: self. INITIAL STATE
+                _, self.initial_state = self.lstm_cell(self.image_embeddings, self.zero_state) # not using output value.
+                lstm_scope.reuse_variables()
+                
+                
+                if self.is_training is True:
+                    seq_len = tf.reduce_sum(self.input_mask, 1)
+                    lstm_output, state = tf.nn.dynamic_rnn(cell = self.lstm_cell,
+                                                       inputs = self.seq_embeddings,
+                                                       sequence_length = seq_len,
+                                                       initial_state = self.initial_state,
+                                                       dtype = tf.float32,
+                                                       scope = lstm_scope)
+                else:
+                    # (512 + 512) = (c + h)
+                    self.state_feed = tf.placeholder(dtype=tf.float32, shape=[None, sum(self.lstm_cell.state_size)], name="state_feed")
+                    # (512, 512)
+                    state_tuple = tf.split(value=self.state_feed, num_or_size_splits=2, axis=1)
 
-	def build_model(self):
-		loss = 0
+                    # Run a single LSTM step.
+                    # PREDICT PHASE: self. STATE_TUPLE
+                    lstm_output, self.state_tuple = self.lstm_cell(inputs=tf.squeeze(self.seq_embeddings, axis=1), state=state_tuple)
+                    #lstm_output, state_tuple = lstm_cell(inputs=self.seq_embeddings[:,0,:], state=initial_state)
 
+            self.lstm_output = tf.reshape(lstm_output, [-1, self.lstm_cell.output_size]) # (500, 512)
+        
+            
+            # for LOSS with MASK
+            if self.is_training is True:
+                weights = tf.to_float(tf.reshape(self.input_mask,[-1])) # MASK WEIGHTS
+                targets = tf.reshape(self.target_seqs, [ -1]) # TARGET
+        
+            with tf.variable_scope('logits') as logits_scope:
+                self.logits = tf.contrib.layers.fully_connected(
+                    inputs = self.lstm_output, 
+                    num_outputs = self.n_words,
+                    activation_fn = None, 
+                    weights_initializer = self.initializer, 
+                    scope = logits_scope)
+                
+            if self.is_training is True:
+                self.loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = targets, logits = self.logits)
+                self.batch_loss = tf.div(tf.reduce_sum(tf.multiply(self.loss, weights)), tf.reduce_sum(weights), name = 'batch_loss')
+                update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+                with tf.control_dependencies(update_ops):
+                    self.optimizer = tf.train.AdamOptimizer(learning_rate=(1e-3), epsilon=1e-3)
+            
+                self.train_op = self.optimizer.minimize(self.batch_loss, global_step=self.global_step_tensor)
+                
+            else:
+                self.soft_logits = tf.nn.softmax(self.logits, name="softmax")
+                self.predict = tf.argmax(self.soft_logits, axis=1)
+                
+                
+             
+    def train(self, img_features, captions):
+        tf.reset_default_graph()
+        
+        with tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True)), graph=self.graph) as sess:
+            
+            init = tf.global_variables_initializer()
+            sess.run(init)
+            
+            total_batch = int(img_features.shape[0] // self.batch_size)     
+            
+            print("Total EPOCH: %d" % self.epoch)
+            best_loss = 1000
+            for e in range(self.epoch):
+                # for debug 
+                # import ipdb
+                # ipdb.set_trace()
+                for b in range(total_batch):
+                    before = b * self.batch_size
+                    after = before + self.batch_size
+                    _, loss, a = sess.run([self.train_op, self.batch_loss, self.lstm_output],
+                                              feed_dict={
+                                                 self.img_features: img_features[before: after],
+                                                 self.input_seqs: captions[before: after, :16],
+                                                 self.target_seqs: captions[before: after, 1:],
+                                                 self.input_mask: 1 * (captions[before: after, :16] > 0)
+                                                 })
+                if e % 5 == 0:
+                    print("LOCAL EPOCH : %d, LOSS: %.4f" %(e,loss))
+                
+            # SAVE the MODEL
+            saver = tf.train.Saver(max_to_keep=3)
+            saver.save(sess, "./models_captioning/captioning_model", global_step=self.global_step_tensor.eval())
+        
+            print("Training done!")
 
-	def predict(self):
-		captions = None
+            
+    def prediction(self, sess, img_feature, word_to_idx, idx_to_word): # img_feature: (512,)
+        
+        captions = []
+        end_mark = word_to_idx["<END>"]
+        
+        
+        for e in range(img_feature.shape[0]):
+            caption = []
+            
+            initial_state = sess.run(self.initial_state, feed_dict={self.img_features: img_feature[e: e+1]})
+            state = np.hstack((initial_state.c, initial_state.h))
+        
+            word = word_to_idx["<START>"]
+        
+            for _ in range(self.maxlen - 1):
+                caption.append(word)
+                if word is end_mark:
+                    break
+
+                word, state = sess.run([self.predict, self.state_tuple],
+                                       feed_dict={
+                                           self.state_feed: state,
+                                           self.input_seqs: np.array([[word]])})
+                state = np.hstack((state.c, state.h))
+                word = word[0]
+                
+            captions.append(caption)
+            
+        return np.array(captions)
+
 
 
--- Assignment3/char_rnn.py
+++ Assignment3/char_rnn.py
@@ -1,6 +1,7 @@
 # This library is used in Assignment3_Part3_CharRNN
 import tensorflow as tf
 from tensorflow.contrib import rnn
+from tensorflow.contrib import legacy_seq2seq
 import numpy as np
 
 # RNN model class definition
@@ -21,29 +22,88 @@ class Model():
         remember: try to implement the model yourself first, and consider the original code as a last resort
         we all copy someone's code sure, but think hard before you copy. it's good for you. i mean REALLY.
         """
-        
+        # choose different rnn cell 
+        if args.model == 'rnn':
+            cell_fn = rnn.RNNCell
+        elif args.model == 'gru':
+            cell_fn = rnn.GRUCell
+        elif args.model == 'lstm':
+            cell_fn = rnn.LSTMCell
+        elif args.model == 'nas':
+            cell_fn = rnn.NASCell
+        else:
+            raise Exception("model type not supported: {}".format(args.model))
+
+        cells = []
+        for _ in range(args.num_layers):
+            cell = cell_fn(args.rnn_size)
+            if training and (args.output_keep_prob < 1.0 or args.input_keep_prob < 1.0):
+                cell = rnn.DropoutWrapper(cell,
+                                          input_keep_prob=args.input_keep_prob,
+                                          output_keep_prob=args.output_keep_prob)
+            cells.append(cell)
+        self.cell = cell = rnn.MultiRNNCell(cells, state_is_tuple=True)
+
         # These are some primers that you must implement: explicitly used at the training loop
         # the nomenclature is from the original code for less headaches.
-        self.initial_state = None
-        self.input_data = None
-        self.targets = None
-        self.cost = None
-        self.final_state = None
-        self.train_op = None
+        self.initial_state = cell.zero_state(args.batch_size, tf.float32)
+        self.input_data = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
+        self.targets = tf.placeholder(tf.int32,[args.batch_size,args.seq_length])
+        
+        # softmax output layer, use softmax to classify
+        with tf.variable_scope('rnnlm'):
+            softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
+            softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
+
+        # transform input to embedding
+        embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
+        inputs = tf.nn.embedding_lookup(embedding, self.input_data)
+
+        # dropout beta testing: double check which one should affect next line
+        if training and args.output_keep_prob:
+            inputs = tf.nn.dropout(inputs, args.output_keep_prob)
+
+        # unstack the input to fits in rnn model
+        inputs = tf.split(inputs, args.seq_length, 1)
+        inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
+
+        def loop(prev, _):
+            prev = tf.matmul(prev, softmax_w) + softmax_b
+            prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
+            return tf.nn.embedding_lookup(embedding, prev_symbol)
+
+        outputs, last_state = legacy_seq2seq.rnn_decoder(inputs, self.initial_state, cell, loop_function=loop if not training else None, scope='rnnlm')
+        output = tf.reshape(tf.concat(outputs, 1), [-1, args.rnn_size])
+        self.final_state = last_state
+
+        self.logits = tf.matmul(output, softmax_w) + softmax_b
+        loss = legacy_seq2seq.sequence_loss_by_example([self.logits], [tf.reshape(self.targets, [-1])],[tf.ones([args.batch_size * args.seq_length])])
+        with tf.name_scope('cost'):
+            self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
+        
+                                             
+
+
 
         # the original source code initializes self.lr = tf.Variable(0.0, trainable=False)
         # why zero?! does the model not learn at all? This is for a technical factoring of the original source code
         # the code later assigns the lr in the training loop by sess.run(tf.assign(model.lr, args.learning_rate * (args.decay_rate ** e)))
         # so let's just use this scheme for less confusions
         self.lr = tf.Variable(0.0, trainable=False)
+        tvars = tf.trainable_variables()
+        # calculate gradients
+        grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),
+                args.grad_clip)
+        with tf.name_scope('optimizer'):
+            optimizer = tf.train.AdamOptimizer(self.lr)
+        self.train_op = optimizer.apply_gradients(zip(grads, tvars))
 
         # And these member variables are explicitly used from sample() function call below from the original code
         # hint: feeling lost? try googling what member functions self.cell is calling, like zero_state for example.
         # we always do it right?
         # and ofc, if you feel that these variables are ugly, you can just use your code scheme instead of these ones.
         # just make sure that the sample() works
-        self.probs = None
-        self.cell = None
+        self.probs = tf.nn.softmax(self.logits)
 
     def sample(self, sess, chars, vocab, num=200, prime='The '):
         """
@@ -62,6 +122,30 @@ class Model():
         """
         
         # the final sentence is defined "ret" in the original source code.
-        ret = None
-        return ret
+        state = sess.run(self.cell.zero_state(1, tf.float32))
+        for char in prime[:-1]:
+            x = np.zeros((1, 1))
+            x[0, 0] = vocab[char]
+            feed = {self.input_data: x, self.initial_state: state}
+            [state] = sess.run([self.final_state], feed)
+
+        def weighted_pick(weights):
+            t = np.cumsum(weights)
+            s = np.sum(weights)
+            return(int(np.searchsorted(t, np.random.rand(1)*s)))
+
+        ret = prime
+        char = prime[-1]
+        for _ in range(num):
+            x = np.zeros((1, 1))
+            x[0, 0] = vocab[char]
+            feed = {self.input_data: x, self.initial_state: state}
+            [probs, state] = sess.run([self.probs, self.final_state], feed)
+            p = probs[0]
+            sample = weighted_pick(p)
+
+            pred = chars[sample]
+            ret += pred
+            char = pred
+        return ret        
     
